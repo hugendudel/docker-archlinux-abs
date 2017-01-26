83cc39cbef76b4a7c3b96d13bf923225c951ccd3
Harm Endres <harm.endres@inovex.de>
2017-01-26 20:08:51 +0100

    local build mirror

--- check-base.sh
+++ check-base.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Server = http://archpkg.hendr.es/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
-pacman -Sy
-pacman -Qu
+pacman -Sy --noprogressbar
+pacman -Qu --noprogressbar
--- check-package-updates.sh
+++ check-package-updates.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 echo "Server = http://archpkg.hendr.es/\$repo/os/\$arch" > /etc/pacman.d/mirrorlist
-pacman -Sy
+pacman --noprogressbar -Sy
 if [[ $(pacman -Qu) && -n ${output_repo} ]]
 then
-  pacman -S --needed --noconfirm git
+  pacman -S --noprogressbar --needed --noconfirm git
   git clone ${output_repo} ${output_repo}-output
-  pacman -Qu | tee ${output_repo}-output/update_history
+  pacman -Qu --noprogressbar | tee ${output_repo}-output/update_history
   cd ${output_repo}-output
   git config --global user.email $git_user_email
   git config --global user.name $git_user_name
